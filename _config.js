f0907ad7a5d005c86555a34a0a1bf37f997e4def
elliotgrigor <elliot.grigor@protonmail.com>
2024-06-09 19:43:43 +0100

    initial commit

---
+++ _config.js
@@ -0,0 +1,27 @@
+import lume from "https://deno.land/x/lume@v2.2.1/mod.ts";
+import inline from "https://deno.land/x/lume@v2.2.1/plugins/inline.ts";
+
+import { minify } from "npm:html-minifier-terser@7.2.0";
+
+const site = lume({
+  src: "./src",
+});
+
+site.use(inline());
+site.copy("static/favicon.ico", "favicon.ico");
+
+const minifyHTML = async function(pages) {
+  for (const page of pages) {
+    page.content = await minify(page.content, {
+      html5: true,
+      quoteCharacter: '"',
+      collapseWhitespace: true,
+      keepClosingSlash: true,
+      removeComments: true,
+    });
+  }
+}
+
+site.process([".html"], minifyHTML);
+
+export default site;
